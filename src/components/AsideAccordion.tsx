b42fa4fe7c43a342c0b8bc865f0f66e56c54cdeb
shibnev <shibnev.anton@gmail.com>
2024-12-22 10:53:29 +0100

    feat

--- src/components/AsideAccordion.tsx
+++ src/components/AsideAccordion.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import { children, className } from '@/types';
+import Image from 'next/image';
 import { useState } from 'react';
+import ArrowIcon from '../../public/icons/arrow.svg';
 
 interface IAsideAccordionProps {
   header?: string
@@ -20,8 +22,16 @@ export default function AsideAccordion({ className, header, children, isOpen = f
           className='px-4 py-2 cursor-pointer text-sm lowercase'
           onClick={() => setActive(!isActive)}
         >
+          <Image
+            src={ArrowIcon}
+            alt='arrow'
+            width={10}
+            height={10}
+            className={`transform ${isActive ? 'rotate-90' : ''}`}
+          />
           {header}
         </div>
+
         {isActive && (
           <div className='px-4 py-2 text-sm '>
             {children}
