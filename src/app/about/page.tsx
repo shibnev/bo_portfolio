235745f0bb764bdcc478ca2b7a5b68a6cb12f1c0
shibnev <shibnev.anton@gmail.com>
2024-12-28 17:33:22 +0100

    feat: add loading state to About and Home components during data fetching

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,29 +9,37 @@ import { IMenuProps } from '@/components/AccordionList';
 export default function About() {
   const [content, setContent] = useState<string | null>(null);
   const [contacts, setContacts] = useState<IMenuProps | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     async function fetchData() {
-      const dataPage = await fetchDataFromFirebase('pages');
-      const dataContacts = await fetchDataFromFirebase('contacts');
+      try {
+        setIsLoading(true);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+        const dataPage = await fetchDataFromFirebase('pages');
+        const dataContacts = await fetchDataFromFirebase('contacts');
 
-      const content = dataPage?.about?.content;
-      if (typeof content === 'string') {
-        setContent(content);
-      } else {
-        setContent(null);
+        const content = dataPage?.about?.content;
+        if (typeof content === 'string') {
+          setContent(content);
+        } else {
+          setContent(null);
+        }
+        setContacts(dataContacts as unknown as IMenuProps);
       }
-      setContacts(dataContacts as unknown as IMenuProps);
     }
 
     fetchData();
-  }, [content, contacts]);
+  }, [content, contacts, isLoading]);
 
   return (
     <PageInner menu={contacts || undefined} >
       <div className='flex flex-col gap-4'>
         <h2>_about_me</h2>
-        {content && (<CodeText>{content}</CodeText>)}
+        {isLoading ? (<p>loading...</p>) : (<CodeText>{content}</CodeText>)}
 
         <a
           className='mt-4 text-sm text-white bg-warning px-8 py-4 rounded-md w-max hover:bg-primaryLight transition-colors hover:text-primaryDark'
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,63 +14,74 @@ export default function Home() {
   const [name, setName] = useState<string>('');
   const [content, setContent] = useState<string[]>([]);
   const [github, setGithub] = useState<{ href: string } | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     async function fetchData() {
-      const dataMe = await fetchDataFromFirebase('me');
-      const dataPage = await fetchDataFromFirebase('pages') as PageData;
-      const dataSocials = await fetchDataFromFirebase('socials');
+      try {
+        setIsLoading(true);
+      } catch (error) { console.error(error); }
+      finally {
+        setIsLoading(false);
+        const dataMe = await fetchDataFromFirebase('me');
+        const dataPage = await fetchDataFromFirebase('pages') as PageData;
+        const dataSocials = await fetchDataFromFirebase('socials');
 
-      setName(dataMe.name);
-      setContent(Array.isArray(dataPage?.main?.content) ? dataPage?.main?.content ?? [] : []);
-      setGithub(dataSocials?.github as { href: string } | null);
+        setName(dataMe.name);
+        setContent(Array.isArray(dataPage?.main?.content) ? dataPage?.main?.content ?? [] : []);
+        setGithub(dataSocials?.github as { href: string } | null);
+      }
     }
 
     fetchData();
-  }, [name, content, github]);
+  }, [name, content, github, isLoading]);
 
 
   return (
     <Container className='relative z-10 py-4 md:py-8 flex-1 flex flex-col'>
       <Blob />
       <div className='flex flex-col gap-4 md:gap-10 h-full flex-1'>
-        <div className='flex flex-col gap-2 justify-center align-middle flex-1'>
-          <h1 className='text-6xl'>{name}</h1>
-          <h3 className='text-xl text-secondary'>{content[0]}</h3>
-        </div>
+        {isLoading ? <p>loading...</p> : (
+          <>
+            <div className='flex flex-col gap-2 justify-center align-middle flex-1'>
+              <h1 className='text-6xl'>{name}</h1>
+              <h3 className='text-xl text-secondary'>{content[0]}</h3>
+            </div>
 
-        <div className='mb-12 md:mb-24 flex flex-col gap-4'>
-          <p className='text-sm text-primaryLight'>{content[1]}</p>
+            <div className='mb-12 md:mb-24 flex flex-col gap-4'>
+              <p className='text-sm text-primaryLight'>{content[1]}</p>
 
-          <p className='text-sm'>
-            {github && (
-              <>
-                <span
-                  className='text-success'
-                >
-                  const
-                </span>
-                {' '}
-                <span
-                  className='text-secondary'
-                >
-                  GITHUB_LINK
-                </span>
-                {' = '}
-                <a
-                  className='text-danger hover:text-white transition-colors'
-                  href={github.href}
-                >
-                  “<span
-                    className='underline'
-                  >
-                    {github.href}
-                  </span>”
-                </a>
-              </>)
-            }
-          </p>
-        </div>
+              <p className='text-sm'>
+                {github && (
+                  <>
+                    <span
+                      className='text-success'
+                    >
+                      const
+                    </span>
+                    {' '}
+                    <span
+                      className='text-secondary'
+                    >
+                      GITHUB_LINK
+                    </span>
+                    {' = '}
+                    <a
+                      className='text-danger hover:text-white transition-colors'
+                      href={github.href}
+                    >
+                      “<span
+                        className='underline'
+                      >
+                        {github.href}
+                      </span>”
+                    </a>
+                  </>)
+                }
+              </p>
+            </div>
+          </>
+        )}
       </div>
     </Container>
   )
