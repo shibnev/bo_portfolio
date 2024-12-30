48885816920bad1f029040e3f8d90ae6ff444ed5
shibnev <shibnev.anton@gmail.com>
2024-12-30 17:57:05 +0100

    sucks

--- firebase.ts
+++ firebase.ts
@@ -2,14 +2,24 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+//   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+// };
+
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCL_WeOmOWqhzSxpXH23EZXcJE5kd1HhMQ",
+  authDomain: "shibnev-portfolio.firebaseapp.com",
+  projectId: "shibnev-portfolio",
+  storageBucket: "shibnev-portfolio.firebasestorage.app",
+  messagingSenderId: "296167085043",
+  appId: "1:296167085043:web:cdc1823bc9fee270b1f886",
+  measurementId: "G-4JVEJWZH4P"
 };
 
 // Initialize Firebase
